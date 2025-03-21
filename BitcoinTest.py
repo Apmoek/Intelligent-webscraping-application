d0be37a97850b192272495edc40d8d5ef7a89cd8
Jeffrey Horst <571214@student.fontys.nl>
2025-03-21 15:43:47 +0100

    Introduced SQLite and added database to ignore.

--- BitcoinTest.py
+++ BitcoinTest.py
@@ -8,6 +8,7 @@ import requests
 import pandas as pd
 from datetime import datetime
 import os
+import sqlite3
 
 # Main code to retrieve actual Bitcoin price
 url = f"https://api.coingecko.com/api/v3/simple/price?ids={ticker}&vs_currencies={currency}&x_cg_demo_api_key={api_key}"
@@ -38,9 +39,13 @@ file_exists = os.path.isfile(filename)
 # Append to CSV file
 df.to_csv(filename, mode='a', index=False, header=not file_exists)
 
-print("✅ Data saved to bitcoin_price.csv")
+# Create or append to SQLite database
+conn = sqlite3.connect("bitcoin_data.db")
+df.to_sql("prices", conn, if_exists="append", index=False)
+conn.close()
 
-
+# Terminal messag, when script has been completed.
+print(f"✅ Data appended to {filename} and stored in bitcoin_data.db")
 
 
 
