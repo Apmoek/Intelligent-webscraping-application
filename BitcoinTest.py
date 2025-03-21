b7d411abc1baf28264047cd32b217e75bdac74c4
Jeffrey Horst <571214@student.fontys.nl>
2025-03-21 14:52:27 +0100

    Uploaded the orginal file and changed the name of the old one.

---
+++ BitcoinTest.py
@@ -0,0 +1,54 @@
+# Importing variables from config file.
+from config import api_key
+from config import ticker
+from config import currency
+
+# Importing Python libs
+import requests
+import pandas as pd
+from datetime import datetime
+import os
+
+# Main code to retrieve actual Bitcoin price
+url = f"https://api.coingecko.com/api/v3/simple/price?ids={ticker}&vs_currencies={currency}&x_cg_demo_api_key={api_key}"
+headers = {"accept": "application/json"}
+response = requests.get(url, headers=headers)
+
+# Convert respone to JSON
+data = response.json()
+
+# Add timestamp
+timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+# Create a DataFrame
+df = pd.DataFrame([{
+    "timestamp": timestamp,
+    "ticker": ticker,
+    "currency": currency,
+    "price": data[ticker][currency],
+    "source": "CoinGecko"
+}])
+
+# Define filename
+filename = "bitcoin_price.csv"
+
+# Check if file exists
+file_exists = os.path.isfile(filename)
+
+# Append to CSV file
+df.to_csv(filename, mode='a', index=False, header=not file_exists)
+
+print("✅ Data saved to bitcoin_price.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
